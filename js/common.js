e114f7b2d4eba6ba7f7e8cc591ad4c2731f32dea
Dmitry <irvis@imega.ru>
2016-09-10 18:40:45 +0300

    issue #19 Добавил лог геолокации

--- js/common.js
+++ js/common.js
@@ -132,9 +132,11 @@ kubikApp.controller('taskCtrl', ['$http', '$location', '$scope', '$timeout', fun
                 } else if(error.TIMEOUT){
                     this.geolocationErr = "hmmm we timed out trying to find where you are hiding!";
                 }
+                console.log(this.geolocationErr);
                 this.geolocation = false;
             }.bind(this), function() {
                 this.geolocationErr = 'Hi there! we are trying to locate you but you have not answered the security question yet.\n\nPlease choose "Share My Location" to enable us to find you.';
+                console.log(this.geolocationErr);
                 this.geolocation = false;
             }.bind(this), 7000, {maximumAge:10000, timeout:0});
     };
