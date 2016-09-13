c52840722014cfb36e2e2b5ea7e1fce58080edbc
Dmitry <irvis@imega.ru>
2016-09-13 21:47:09 +0300

    add log

--- js/common.js
+++ js/common.js
@@ -136,7 +136,7 @@ kubikApp.controller('taskCtrl', ['$http', '$location', '$scope', '$timeout', fun
                 } else if(error.TIMEOUT){
                     this.geolocationErr = "hmmm we timed out trying to find where you are hiding!";
                 }*/
-                this.geolocationErr = 'sdfsdfsdf';
+                this.task.geolocationErr = 'sdfsdfsdf';
                 this.geolocationWork = false;
                 $scope.$applyAsync();
             }.bind(this),
